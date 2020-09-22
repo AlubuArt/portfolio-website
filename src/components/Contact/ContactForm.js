e3f43ab6c351ca36f53bac242f736e05930c93b5
AlubuArt <57637214+AlubuArt@users.noreply.github.com>
2020-09-22 17:22:34 +0200

    added contact form

--- src/components/Contact/ContactForm.js
+++ src/components/Contact/ContactForm.js
@@ -3,20 +3,52 @@ import './ContactForm.css'
 
 function ContactForm() {
     return (
-        <div className="contact-container">
-            <div className="contact-form-container">
-                <div className="form-header">
-                    
-                </div>
+<div className="contact-main-container">
+<div className="contact-container">
+  <form>
+    <h1 className="form-header">Contact</h1>
+  <div className="name-section">
+    <div className="col-25">
+      <label for="name">Name</label>
+    </div>
+    <div className="col-75">
+      <input type="text" id="name" name="name" placeholder=""></input>
+    </div>
+  </div>
+  
+  <div className="email-section">
+    <div className="col-25">
+      <label for="email">Email</label>
+    </div>
+    <div className="col-75">
+      <input type="text" id="email" name="email" placeholder=""></input>
+    </div>
+  </div>
+  <div className="subject-section">
+    <div className="col-25">
+      <label for="subject">Subject</label>
+    </div>
+    <div className="col-75">
+      <input type="text" id="subject" name="subject"></input>
+    </div>
+  </div>
+  <div className="message-section">
+    <div className="col-25">
+      <label for="messsage">Message</label>
+    </div>
+    <div className="col-75">
+      <textarea id="Message" name="subject" placeholder="Write something.." ></textarea>
+    </div>
+  </div>
+  <div className="submit-section">
+    <input type="submit" value="SUBMIT"></input>
+  </div>
+  </form>
+</div>
+</div>
+        
 
-
-            </div>
-            <div className="contact-information-container">
-
-            <h1>helle world</h1>
-            </div>
-        </div>
     )
-}
+};
 
 export default ContactForm
