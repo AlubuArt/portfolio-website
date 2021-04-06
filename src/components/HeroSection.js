39e5e4b50d553f1bd742ff489f031d5ff2e3ea62
AlubuArt <jc@jcvisueldesign.dk>
2021-04-06 19:38:38 +0200

    added icons to hero

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -4,6 +4,10 @@ import './HeroSection.css';
 import hero from '../images/header_blaa_2000x1000.jpg';
 import { Link} from 'react-scroll'
 import {FaAngleDown} from 'react-icons/fa';
+import linkedIn from '../images/linkedin-3-32.png';
+import github from '../images/github-11-32.png';
+import emailIcon from '../images/email-32.png';
+import './About.css';
 
 class HeroSection extends Component {
 
@@ -15,16 +19,27 @@ class HeroSection extends Component {
         }
         return (
             <div className="hero-container">
-                <img src={hero} alt="sdf" className="hero-image" />
-                
+                <div className="hero-image">
+                    <img src={hero} alt="sdf" className="hero-image" />
+                </div>
+                <div className="hero-text-section">
                     <h1>{title}</h1>
                     <p>{subtitle}</p>
+                </div>
+                <div className="hero-buttons">
+                <a href="https://www.linkedin.com/in/jacob-christensen-konceptudvikling/" target="_blank">
+                                    <img src={linkedIn} alt="linkedin"></img>
+                                </a>
+                                <a href="https://github.com/AlubuArt" target="_blank">
+                                    <img src={github} alt="github"></img>
+                                </a>
+                                <a href="mailto:jc@jcvisueldesign.dk">
+                                    <img src={emailIcon}  type="email" alt="email"></img>
+                                </a>
+                </div>
                     <Link activeClass="active" to="about" spy={true} smooth={true} offset={50} duration={500} className="scroll-down-button">
-                    <FaAngleDown/>
+                        <FaAngleDown/>
                     </Link>
-                    
-                    
-    
             </div>
             
         )
